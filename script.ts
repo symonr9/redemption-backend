9d1fa311cb60c2a09e06f0956bd735b4e8322bd7
Symon R <symonr@videx.com>
2024-08-16 13:53:02 -0700

    [Setup] Misc updates.

--- script.ts
+++ script.ts
@@ -3,44 +3,14 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
-    // const user = await prisma.user.create({
-    //     data: {
-    //       name: 'Bob',
-    //       email: 'bob@prisma.io',
-    //       posts: {
-    //         create: [
-    //           {
-    //             title: 'Hello World',
-    //             published: true
-    //           },
-    //           {
-    //             title: 'My second post',
-    //             content: 'This is still a draft'
-    //           }
-    //         ],
-    //       },
-    //     },
-    //   })
-    //   console.log(user)
-
-    // const user = await prisma.user.create({
-    //     data: {
-    //       name: 'Alice',
-    //       email: 'alice@prisma.io',
-    //     },
-    //   })
-    //   console.log(user)
-
+  console.log("Running main...");
       
-    const usersWithPosts = await prisma.user.findMany({
-        include: {
-          posts: true,
-        },
-      })
-      console.dir(usersWithPosts, { depth: null })
-
-//   const users = await prisma.user.findMany()
-//   console.log(users)
+  const usersWithPosts = await prisma.user.findMany({
+      include: {
+        posts: true,
+      },
+    })
+    console.dir(usersWithPosts, { depth: null })
 }
 
 main()
