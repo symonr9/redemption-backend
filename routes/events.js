2390f7645e54a06a28b71c23147ee09da0fd8593
symonr9 <symonr12@gmail.com>
2024-08-17 00:38:27 -0700

    [Refactor] Improve route setup.

--- routes/events.js
+++ routes/events.js
@@ -5,10 +5,10 @@ const router = express.Router();
 
 // Get all
 router.get('/', async (req, res) => {
-  try {
-    const items = await prisma.one.findMany();
-    res.json(items);
-  } catch (err) {
-    res.status(500).json({ error: 'Failed to fetch items' });
-  }
+    try {
+        const items = await prisma.one.findMany();
+        res.json(items);
+    } catch (err) {
+        res.status(500).json({ error: 'Failed to fetch items' });
+    }
 });
