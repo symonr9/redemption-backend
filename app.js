c30d78bb3641b5a661f47c02bb810992bb457553
symonr9 <symonr12@gmail.com>
2024-10-02 12:42:32 -0700

    Add middleware to check for fixed auth token and user id via headers instead of as API param.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const routes = require('./routes'); // Automatically picks up the index.js file
 const session = require('express-session');
 const passport = require('./auth/google-oauth');
+const prisma = require('./misc/prisma-client')
 
 const PORT = process.env.PORT || 3000;
 
@@ -28,6 +29,37 @@ app.use(
 // Initialize Passport and restore authentication state, if any, from the session
 app.use(passport.initialize());
 app.use(passport.session());
+
+app.use((req, res, next) => {
+    if (req.headers.fixed_auth_token !== process.env.FIXED_AUTH_TOKEN) {
+        return res.status(400).json({ error: 'Invalid fixed_auth_token' });
+    }
+    next();
+});
+
+app.use(async (req, res, next) => {
+    // White-listed
+    if ([].includes(req.path)) {
+        next();
+        return;
+    }
+
+    const userId = req.headers.user_id;
+    if (!userId) {
+        return res.status(400).json({ error: 'user_id header is required' });
+    }
+
+    const user = await prisma.user.findUnique({
+        where: { id: userId },
+    });
+    if (!user) {
+        return res.status(404).json({ error: 'User not found' });
+    };
+
+    req.user = user;
+
+    next();
+});
 
 // Use the combined routes
 app.use('/', routes);
