6a7c9f022f4a68ee768123743a34786ae6d52124
symonr9 <symonr12@gmail.com>
2024-10-03 22:48:46 -0700

    Implemen endpoint for updating action steps (add, update, and remove).

--- routes/ones.js
+++ routes/ones.js
@@ -8,11 +8,11 @@ router.post('/create', async (req, res) => {
   const { one } = req.body;
   try {
     const result = await prisma.one.create({
-      data: { 
-        name: one.name, 
+      data: {
+        name: one.name,
         icon: one.iconKey,
-        stage: one.stage, 
-        category: one.category, 
+        stage: one.stage,
+        category: one.category,
         userId: user.id
       }
     });
@@ -27,10 +27,10 @@ router.post('/update', async (req, res) => {
   try {
     const result = await prisma.one.update({
       where: { id: one.id },
-      data: { 
-        name: one.name, 
+      data: {
+        name: one.name,
         icon: one.iconKey,
-        stage: one.stage, 
+        stage: one.stage,
         category: one.category,
         gospelChecklist: one.gospelChecklist ? one.gospelChecklist.join(',') : null,
       }
@@ -38,6 +38,62 @@ router.post('/update', async (req, res) => {
     res.status(200).json(result);
   } catch (error) {
     res.status(500).json({ error: error.message });
+  }
+});
+
+router.post('/action-steps/update', async (req, res) => {
+  const { actionSteps, oneId } = req.body;
+
+  try {
+    await prisma.$transaction(async (tx) => {
+      const existingActionSteps = await tx.actionStep.findMany({
+        where: { oneId }
+      });
+
+      const idsToProcess = new Set(existingActionSteps.map(step => step.id));
+
+      for (const actionStep of actionSteps) {
+        if (actionStep.id && idsToProcess.has(actionStep.id)) { // Update
+          await tx.actionStep.update({
+            where: { id: actionStep.id },
+            data: {
+              notes: actionStep.notes ? actionStep.notes : null,
+              isComplete: actionStep.isComplete,
+              targetDate: actionStep.targetDate ? actionStep.targetDate : null,
+              type: actionStep.type
+            }
+          });
+          idsToProcess.delete(actionStep.id);
+        }
+        else {
+          await tx.actionStep.create({
+            data: {
+              notes: actionStep.notes ? actionStep.notes : null,
+              isComplete: actionStep.isComplete,
+              targetDate: actionStep.targetDate ? actionStep.targetDate : null,
+              type: actionStep.type,
+              oneId: oneId
+            }
+          });
+        }
+      }
+
+      // Delete any action steps that were not included in the new list
+      for (const id of idsToProcess) {
+        await tx.actionStep.delete({
+          where: { id }
+        });
+      }
+    });
+
+    const newActionSteps = await prisma.actionStep.findMany({
+      where: { oneId }
+    });
+
+    res.status(200).json(newActionSteps);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: `An error occurred while processing action steps: ${error.message}` });
   }
 });
 
