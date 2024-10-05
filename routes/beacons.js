665b7ee07a16945176905624fb605657db9da6a7
symonr9 <symonr12@gmail.com>
2024-10-04 22:15:36 -0700

    Implement beacons routes for creating, querying beacons, creating and updating beacon activities.

---
+++ routes/beacons.js
@@ -0,0 +1,87 @@
+// routes/prayer.js
+const express = require('express');
+const prisma = require('../misc/prisma-client');
+
+const router = express.Router();
+
+router.get('/active', async (req, res) => {
+    try {
+        const beacons = await prisma.beacon.findMany({
+            where: {
+                activeUntil: {
+                    gte: new Date(),
+                },
+            },
+            include: {
+                activities: true,
+            }
+        });
+        res.json(beacons);
+    } catch (err) {
+        res.status(500).json({ error: 'Failed to fetch beacons' });
+    }
+});
+
+router.post('/create', async (req, res) => {
+    const user = req.user;
+    const { beacon } = req.body;
+    try {
+        const tomorrow = new Date();
+        tomorrow.setDate(tomorrow.getDate() + 1);
+
+        const result = await prisma.beacon.create({
+            data: {
+                name: beacon.name,
+                message: beacon.message ? beacon.message : null,
+                oneId: beacon.oneId,
+                priority: beacon.priority,
+                type: beacon.type,
+                activeUntil: tomorrow,
+                shareOwnName: beacon.shareOwnName,
+                userId: user.id
+            }
+        });
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+router.post('/activity/create', async (req, res) => {
+    const user = req.user;
+    const { activity } = req.body;
+    try {
+        const result = await prisma.beaconActivity.create({
+            data: {
+                note: activity.note,
+                userId: user.id,
+                beaconId: activity.beaconId
+            }
+        });
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+router.post('/activity/update', async (req, res) => {
+    const user = req.user;
+    const { activity } = req.body;
+    try {
+        const result = await prisma.beaconActivity.update({
+            where: {
+                id: activity.id
+            },
+            data: {
+                note: activity.note,
+            }
+        });
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+
+
+module.exports = router;
