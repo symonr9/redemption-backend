7bead9d51800535286fff6fac6a7e7173d615161
symonr9 <symonr12@gmail.com>
2024-10-18 20:58:16 -0700

    Clean up.

--- routes/index.js
+++ routes/index.js
@@ -13,8 +13,6 @@ const routeFiles = fs.readdirSync(__dirname).filter(file => {
 routeFiles.forEach(file => {
     const route = require(path.join(__dirname, file));
     const routeName = file.replace('.js', '');
-
-    console.log("route name: ", routeName, " route - ", route);
     router.use(`/${routeName}`, route);
 });
 
