71e87bcd17105d0618038e8daf1d9c909218fe00
symonr9 <symonr12@gmail.com>
2024-10-22 12:47:18 -0700

    Add tags support for beacons.

--- routes/beacons.js
+++ routes/beacons.js
@@ -66,7 +66,8 @@ router.post('/create', authenticateJwt, async (req, res) => {
                 type: beacon.type,
                 activeUntil: tomorrow,
                 shareOwnName: beacon.shareOwnName,
-                userId: user.id
+                userId: user.id,
+                tags: beacon.tags ? beacon.tags.join(',') : null,
             }
         });
         res.status(200).json(result);
