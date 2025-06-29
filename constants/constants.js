d44ece4e9438aaffdcb659de611ccc1fe9043f32
symonr9 <symonr12@gmail.com>
2025-06-29 12:46:02 -0700

    Adjust count of global and auto beacons.

--- constants/constants.js
+++ constants/constants.js
@@ -2,8 +2,8 @@ module.exports.MAX_LONG_TEXT_LENGTH = 1000;
 module.exports.MAX_NORMAL_TEXT_LENGTH = 255;
 module.exports.MAX_NAME_LENGTH = 24;
 
-module.exports.MAX_NUM_GLOBAL_BEACONS = 5;
-module.exports.MAX_NUM_AUTO_BEACONS = 5;
+module.exports.MAX_NUM_GLOBAL_BEACONS = 3;
+module.exports.MAX_NUM_AUTO_BEACONS = 3;
 
 // Normal beacons
 module.exports.DAYS_ACTIVE_FOR_BEACONS = 1;
