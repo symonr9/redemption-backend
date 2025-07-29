207ae05137517737cdf1c49159ae84551a6d23e5
symonr9 <symonr12@gmail.com>
2025-07-28 21:24:37 -0700

    Change max name length to 32.

--- constants/constants.js
+++ constants/constants.js
@@ -1,6 +1,6 @@
 module.exports.MAX_LONG_TEXT_LENGTH = 1000;
 module.exports.MAX_NORMAL_TEXT_LENGTH = 255;
-module.exports.MAX_NAME_LENGTH = 24;
+module.exports.MAX_NAME_LENGTH = 32;
 
 module.exports.MAX_NUM_GLOBAL_BEACONS = 3;
 module.exports.MAX_NUM_AUTO_BEACONS = 3;
